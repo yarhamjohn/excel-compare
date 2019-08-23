96df3b23273d7740c6d0adc70257a4eafb732b2d
John Yarham <yarhamjohn@gmail.com>
2019-08-23 15:56:35 +0100

    Setup up basic cli with no functionality

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	analyzeCommand := flag.NewFlagSet("analyze", flag.ExitOnError)
+	flattenCommand := flag.NewFlagSet("flatten", flag.ExitOnError)
+
+	// Analyze subcommand flag pointers
+	analyzeFilePath := analyzeCommand.String("filePath", "", "Excel file to analyse. (Required)")
+
+	// Flatten subcommand flag pointers
+	flattenFilePath := flattenCommand.String("filePath", "", "Excel file to analyse. (Required)")
+	delimiter := flattenCommand.String("delimiter", ",", "The field delimiter to use {,|^}. (Required)")
+
+	// Checks a subcommand has been provided
+	// os.Args[0] is the main command, os.Args[1] the subcommand
+	if len(os.Args) < 2 {
+		fmt.Println("One of the subcommands 'analyze' or 'flatten' is required.")
+		os.Exit(1)
+	}
+
+	// Parse the flags
+	switch os.Args[1] {
+	case "analyze":
+		analyzeCommand.Parse(os.Args[2:])
+	case "flatten":
+		flattenCommand.Parse(os.Args[2:])
+	default:
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	// Check which subcommand was parsed
+	if analyzeCommand.Parsed() {
+		if *analyzeFilePath == "" {
+			analyzeCommand.PrintDefaults()
+			os.Exit(1)
+		}
+
+		fmt.Printf("File path: %s", *analyzeFilePath)
+	}
+
+	if flattenCommand.Parsed() {
+		if *flattenFilePath == "" {
+			flattenCommand.PrintDefaults()
+			os.Exit(1)
+		}
+
+		delimiterChoices := map[string]bool{",": true, "^": true, "|": true}
+		if _, validChoice := delimiterChoices[*delimiter]; !validChoice {
+			flattenCommand.PrintDefaults()
+			os.Exit(1)
+		}
+
+		fmt.Printf("The file: %s has been flattened using the delimiter: %s", *flattenFilePath, *delimiter)
+	}
+}
